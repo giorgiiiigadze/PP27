8685e3d0c9bcac9b5534f3262b8e0d8e5709b521
Anniymm <animaisuradze1999@gmail.com>
2025-08-12 20:44:44 +0400

    add - models - forms

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from .models import UserProfile
+from django.contrib.auth.models import User
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
+
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'bio', 'location', 'birth_date']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        
--- project/users/models.py
+++ project/users/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE )
+    profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(blank = True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null = True, blank = True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
