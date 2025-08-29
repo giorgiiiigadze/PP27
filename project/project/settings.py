65cf3f0babcec5fa04bae958a7d862b4331949c7
Anniymm <animaisuradze1999@gmail.com>
2025-08-29 20:23:50 +0400

    add .env

--- project/project/settings.py
+++ project/project/settings.py
@@ -125,6 +125,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PAGINATE_BY = 2
 
 # //////////////////////////////////////////////////////////////////////////////
+from decouple import config
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 #TLS
@@ -132,8 +134,8 @@ EMAIL_USE_TLS = True
 
 # EMAIL_USE_SSL = True - 465
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = '...'
-EMAIL_HOST_PASSWORD = '...'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER') #
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
